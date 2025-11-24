5d734ea428f4fe2059da8788e6728703317544f9
Robert Elliott <jakel2006@me.com>
2025-11-24 15:48:25 +0000

    upd: remove .vscode folder and move app.py to assets folder

--- assets/app.py
+++ assets/app.py
@@ -6,7 +6,7 @@ app = dash.Dash(__name__)
 app.layout = html.Div(
     children=[
         html.Iframe(
-            src="assets/insurance_report.html",  # must be under assets/ to be properly served
+            src="insurance_report.html",  # must be under assets/ to be properly served
             style={"height": "1080px", "width": "100%"},
         )
     ]
